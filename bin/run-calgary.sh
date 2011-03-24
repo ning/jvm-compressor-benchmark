88d012f26f7a9d2123126e2f35de64372baa660f
Tatu <tatu@ning.com>
2011-03-24 15:25:42 -0700

    Add 2 more corpuses (calgary, canterbury)

--- bin/run-calgary.sh
+++ bin/run-calgary.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
  
-echo "About to run full test"
+echo "About to run test on Calgary corpus files"
 
 # Nothing big stored in memory, heap can remain modest 
+# Since there are 18 input files, 2 modes (comp/uncomp),
+# group by 9
+
 java -server -cp lib/japex/\* \
  -Xmx128M \
  -Djava.awt.headless=true \
  -Djapex.runsPerDriver=1 \
- -Djapex.warmupTime=5 \
- -Djapex.runTime=10 \
+ -Djapex.warmupTime=7 \
+ -Djapex.runTime=30 \
  -Djapex.numberOfThreads=1 \
  -Djapex.reportsDirectory=reports \
- -Djapex.plotGroupSize=8 \
- -Djapex.inputDir="testdata/" \
+ -Djapex.plotGroupSize=9 \
+ -Djapex.inputDir="testdata/calgary" \
  com.sun.japex.Japex \
- cfg/tests-all.xml
+ cfg/tests-calgary.xml
 
 echo "Done!";
