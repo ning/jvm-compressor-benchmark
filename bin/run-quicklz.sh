62bd62c4adbcf08ab9b47bbc88238cf6a97469f4
Tatu Saloranta <tsaloranta@gmail.com>
2011-03-25 19:43:02 -0700

    Add test data from http://quicklz.com/bench.html

---
+++ bin/run-quicklz.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+ 
+echo "About to run test on tests from http://quicklz.com"
+
+# 5 test files (dropped NotTheMusic.mp4 since it doesn't compress
+# and we have plenty of such files); group by 5
+java -server -cp lib/japex/\* \
+ -Xmx128M \
+ -Djava.awt.headless=true \
+ -Djapex.runsPerDriver=1 \
+ -Djapex.warmupTime=7 \
+ -Djapex.runTime=30 \
+ -Djapex.numberOfThreads=1 \
+ -Djapex.reportsDirectory=reports/quicklz \
+ -Djapex.plotGroupSize=5 \
+ -Djapex.inputDir="testdata/quicklz" \
+ com.sun.japex.Japex \
+ cfg/tests-quicklz.xml
+
+echo "Done!";
