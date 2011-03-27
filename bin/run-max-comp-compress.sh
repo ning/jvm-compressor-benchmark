2651ad9156cfb8280273839502a320b41bcae4d0
Tatu Saloranta <tsaloranta@gmail.com>
2011-03-26 21:58:12 -0700

    More results

---
+++ bin/run-max-comp-compress.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+ 
+echo "About to run compress-only test with 'maximum compression' test data"
+
+# Nothing big stored in memory, heap can remain modest 
+# Since there are 10 input files, 2 modes (comp/uncomp),
+# group by 5
+java -server -cp lib/japex/\* \
+ -Xmx128M \
+ -Djava.awt.headless=true \
+ -Djapex.runsPerDriver=1 \
+ -Djapex.warmupTime=7 \
+ -Djapex.runTime=30 \
+ -Djapex.numberOfThreads=1 \
+ -Djapex.reportsDirectory=reports/maxcomp-compress \
+ -Djapex.plotGroupSize=5 \
+ -Djapex.inputDir="testdata/maximumcompression" \
+ com.sun.japex.Japex \
+ cfg/tests-max-comp-compress.xml
+
+echo "Done!";
---
+++ bin/run-max-comp-uncompress.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+ 
+echo "About to run uncompress-only test with 'maximum compression' test data"
+
+# Nothing big stored in memory, heap can remain modest 
+# Since there are 10 input files, 2 modes (comp/uncomp),
+# group by 5
+java -server -cp lib/japex/\* \
+ -Xmx128M \
+ -Djava.awt.headless=true \
+ -Djapex.runsPerDriver=1 \
+ -Djapex.warmupTime=7 \
+ -Djapex.runTime=30 \
+ -Djapex.numberOfThreads=1 \
+ -Djapex.reportsDirectory=reports/maxcomp-uncompress \
+ -Djapex.plotGroupSize=5 \
+ -Djapex.inputDir="testdata/maximumcompression" \
+ com.sun.japex.Japex \
+ cfg/tests-max-comp-uncompress.xml
+
+echo "Done!";
