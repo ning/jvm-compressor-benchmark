b0876e37df0f2615e278a582532df835e04e159b
Tatu Saloranta <tsaloranta@gmail.com>
2011-04-04 21:18:51 -0700

    ...

---
+++ bin/run-canterbury-compress.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+ 
+echo "About to run test on Canterbury corpus files"
+
+# Nothing big stored in memory, heap can remain modest 
+# Since there are 11 input files, 2 modes (comp/uncomp),
+# group by... hmmh. 5.5 would be optimal. But I guess 6 has to do?
+
+java -server -cp lib/japex/\* \
+ -Xmx128M \
+ -Djava.awt.headless=true \
+ -Djapex.runsPerDriver=1 \
+ -Djapex.warmupTime=7 \
+ -Djapex.runTime=30 \
+ -Djapex.numberOfThreads=1 \
+ -Djapex.reportsDirectory=reports/canterbury-compress \
+ -Djapex.plotGroupSize=6 \
+ -Djapex.inputDir="testdata/canterbury" \
+ com.sun.japex.Japex \
+ cfg/tests-canterbury-compress.xml
+
+echo "Done!";
