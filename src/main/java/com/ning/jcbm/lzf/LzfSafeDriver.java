f6cde62b03d5451ad87ed023755a077c47f75d50
Tatu Saloranta <tsaloranta@gmail.com>
2013-03-05 17:37:13 -0800

    Update drivers, add new LZF-safe, comment out some less useful from 'all'

--- src/main/java/com/ning/jcbm/lzf/LzfSafeDriver.java
+++ src/main/java/com/ning/jcbm/lzf/LzfSafeDriver.java
@@ -5,32 +5,33 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 import com.ning.compress.lzf.*;
+import com.ning.compress.lzf.util.ChunkDecoderFactory;
+import com.ning.compress.lzf.util.ChunkEncoderFactory;
 
 import com.ning.jcbm.DriverBase;
 
-public class LzfDriver extends DriverBase
+public class LzfSafeDriver extends DriverBase
 {
-    public LzfDriver() {
-        super("LZF");
+    public LzfSafeDriver() {
+        super("LZF-safe");
     }
     
     protected int compressBlock(byte[] uncompressed, byte[] compressBuffer) throws IOException
     {
-        // uses new "appendEncoded" in 0.9.7:
-        int outPtr = LZFEncoder.appendEncoded(uncompressed, 0, uncompressed.length,
+        int outPtr = LZFEncoder.safeAppendEncoded(uncompressed, 0, uncompressed.length,
                 compressBuffer, 0);
         return outPtr;
     }
 
     protected int uncompressBlock(byte[] compressed, byte[] uncompressBuffer) throws IOException
     {
-        return LZFDecoder.decode(compressed, 0, compressed.length, uncompressBuffer);
+        return LZFDecoder.safeDecode(compressed, 0, compressed.length, uncompressBuffer);
     }
 
     protected void compressToStream(byte[] uncompressed, OutputStream rawOut)
         throws IOException
     {
-        LZFOutputStream out = new LZFOutputStream(rawOut);
+        LZFOutputStream out = new LZFOutputStream(ChunkEncoderFactory.safeInstance(1 << 16), rawOut);
         out.write(uncompressed);
         out.close();
     }
@@ -38,7 +39,7 @@ public class LzfDriver extends DriverBase
     protected int uncompressFromStream(InputStream compIn, byte[] inputBuffer)
         throws IOException
     {
-        LZFInputStream in = new LZFInputStream(compIn);
+        LZFInputStream in = new LZFInputStream(ChunkDecoderFactory.safeInstance(), compIn);
 
         int total = 0;
         int count;
