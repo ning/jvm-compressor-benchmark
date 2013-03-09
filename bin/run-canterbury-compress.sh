7700ba9a1259c114b56c92b04ac53ca0889834b7
Tatu Saloranta <tsaloranta@gmail.com>
2013-03-09 11:21:44 -0800

    increase heap for one of tests

--- bin/run-canterbury-compress.sh
+++ bin/run-canterbury-compress.sh
@@ -2,12 +2,12 @@
  
 echo "About to run test on Canterbury corpus files"
 
-# Nothing big stored in memory, heap can remain modest 
+# LZO has some issues with memory usage, apparently need to increase?
 # Since there are 11 input files, 2 modes (comp/uncomp),
 # group by... hmmh. 5.5 would be optimal. But I guess 6 has to do?
 
 java -server -cp lib/japex/\* \
- -Xmx400M \
+ -Xmx500M \
  -Djava.awt.headless=true \
  -Djapex.runsPerDriver=1 \
  -Djapex.warmupTime=7 \
